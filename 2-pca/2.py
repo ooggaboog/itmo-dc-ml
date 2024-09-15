45e3dfa8e25bacb8a55cce0979cca1e80ede0066
ooggaboog <becoolgorchinskaya@gmail.com>
2024-09-16 00:05:28 +0300

    2-pca

--- 2-pca/2.py
+++ 2-pca/2.py
@@ -2,8 +2,8 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-scores = pd.read_csv('2-data1.csv', header=None, sep=';')
-weights = pd.read_csv('2-data2.csv', header=None, sep=';')
+scores = pd.read_csv('https://courses.openedu.ru/assets/courseware/v1/453d2c47ea4f165db9546c68e00f24de/asset-v1:ITMOUniversity+MLDATAN+spring_2024_ITMO_bac+type@asset+block/X_reduced_792.csv', header=None, sep=';')
+weights = pd.read_csv('https://courses.openedu.ru/assets/courseware/v1/78f586bc1239cec75ef777a7816d7b83/asset-v1:ITMOUniversity+MLDATAN+spring_2024_ITMO_bac+type@asset+block/X_loadings_792.csv', header=None, sep=';')
 
 # восстановление изображения по первым 10 главным компонентам
 restored_image = np.dot(scores, weights.T)
